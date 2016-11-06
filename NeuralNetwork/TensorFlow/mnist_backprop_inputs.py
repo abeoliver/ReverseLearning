7441f41d9952d21b0cd86ff4b47e8ca7a5eb7faa
Abraham Oliver <abeoliver.116@gmail.com>
2016-11-06 15:39:44 -0500

    Small changes

---
+++ NeuralNetwork/TensorFlow/mnist_backprop_inputs.py
@@ -0,0 +1,8 @@
+# MNIST Backprop To Find Optimal Input
+# Abraham Oliver, 2016
+
+# General structure from tensorflow.org
+# Matplotlib help from http://tneal.org/post/tensorflow-ipython/TensorFlowMNIST/
+
+# Import Tensorflow
+import tensorflow as tf
