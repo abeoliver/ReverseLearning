caddfac50068c8a0f2917f329ef167c73cb6535a
Abraham Oliver <abeoliver.116@gmail.com>
2016-11-22 19:07:23 -0800

    Began module

---
+++ NeuralNetwork/Network.py
@@ -0,0 +1,24 @@
+# Network Module
+# Abraham Oliver, 2016
+# ReverseLearning
+
+# Import dependencies
+import tensorflow as tf
+import numpy as np
+
+class Network (object):
+    """
+    A neural network using tensorflow for training with InputBackprop methods
+
+    METHODS:
+        -
+
+    ATTRIBUTES:
+        -
+    """
+    def __init__(self, layers):
+        """
+
+        :param layers:
+        """
+        pass
