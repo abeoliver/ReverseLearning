33dc1e5a9b82d4690ca66211df88ae2b586e7a1b
Abraham Oliver <abeoliver.116@gmail.com>
2016-11-23 21:54:40 -0800

    Input Backprop (no clue why it was branch because it is the project) (UNFINISHED) (#12)
    * Started backprop research for optimizing input
    
    * Small changes
    
    * Misc
    
    * Fixed Softmax tutorial bugs
    
    * Notebook Improvements
    
    * Input Backprop progress
    
    * Image backprop wor
    
    * Tried jupyter on cloud9
    
    * Experimentation
    
    * MNIST Trials (UNCOMPLETED) (#11)
    
    * Small experiments created
    
    Started 3 input addition network, fighting with datatypes
    
    * Addition network written
    
    * Experimental notebook added
    
    * Tried longer training loop
    
    * Started backprop research for optimizing input
    
    * Small changes
    
    * Misc
    
    * Fixed Softmax tutorial bugs
    
    * Notebook Improvements
    
    * Input Backprop progress
    
    * Image backprop wor
    
    * Tried jupyter on cloud9
    
    * Experimentation
    
    * MNIST Trials (UNCOMPLETED) (#11)
    
    * Small experiments created
    
    Started 3 input addition network, fighting with datatypes
    
    * Addition network written
    
    * Experimental notebook added
    
    * Tried longer training loop

--- NeuralNetwork/TensorFlow/mnist_backprop_inputs.py
+++ NeuralNetwork/TensorFlow/mnist_backprop_inputs.py
@@ -5,6 +5,10 @@
 # Matplotlib help from http://tneal.org/post/tensorflow-ipython/TensorFlowMNIST/
 
 # Import Tensorflow
+<<<<<<< HEAD
 import tensorflow as tf
 # Import Numpy
 import numpy as np
+=======
+import tensorflow as tf
+>>>>>>> InputBackprop
