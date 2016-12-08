3817c344caf4eb6bf0d9121fd764782e33d64d6b
Abraham Oliver <abeoliver.116@gmail.com>
2016-12-07 20:53:23 -0500

    Fixed training, added TODOs

--- NeuralNetwork/ModuleExample.py
+++ NeuralNetwork/ModuleExample.py
@@ -3,8 +3,24 @@
 # Reverse Learning
 
 import tensorflow as tf
+import numpy as np
 from Network import Network
 
-n = Network([1, 1])
+from random import random, randint
+def newSet(size, inputs = 3):
+    data = []
+    labels = []
+    for s in range(size):
+        newInputs = [randint(-10, 10) for i in range(inputs)]
+        data.append(newInputs)
+        labels.append([sum(newInputs)])
+    return (data, labels)
+
+n = Network([3, 1])
 n.initWeights(mode="zeros")
-print n.feed([1.0]).eval(session=n._session)
+n.train(newSet(100, inputs = 3), 2001, .01, debug = True, debug_interval = 500)
+p = newSet(1)
+a = n.feed(p[0])
+print p[0]
+print a.eval(session = n._session)
+print p[1]
