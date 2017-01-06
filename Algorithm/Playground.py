036ad83f991146d071f63a79ca1c2f7a167deb6d
Abraham Oliver <abeoliver.116@gmail.com>
2017-01-06 18:16:56 -0500

    Began work on the NewsPopularity dataset to test new module features

--- Algorithm/Playground.py
+++ Algorithm/Playground.py
@@ -2,3 +2,10 @@
 # ReverseLearning
 
 from Network import Network
+
+# Import Dataset
+import csv
+with open("../DataSets/OnlineNewsPopularity/OnlineNewsPopularity.csv", 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print row
