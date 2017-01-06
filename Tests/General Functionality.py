481b8a71b66a10a274a8a9f1d9b9ce0b86392d19
Abraham Oliver <abeoliver.116@gmail.com>
2017-01-06 00:03:26 -0500

    General tests finished

--- Tests/General Functionality.py
+++ Tests/General Functionality.py
@@ -16,29 +16,68 @@ Configurations vary in learning rate, epochs, number of inputs, and size of targ
 """
 
 
-def AdditionDataSet(size):
-    """
-    data = multiple floats between -100 and 100
-    label = the sum of the floats
-    """
-    data = []
-    labels = []
-    for s in range(size):
-        newInputs = [random() * randint(-100, 100) for i in range(ins)]
-        data.append(newInputs)
-        labels.append([sum(newInputs)])
-    return (data, labels)
-
-
 # Trial: 1
 # Inputs: 2
 # Learning Rate: .001
 # Epochs: 10000
 # Target: 4.0
-
-# Feed-forward
+# FEED FORWARD
 n = Network([2, 1])
 n.initWeights(mode="ones")
 n.initBiases(mode="zeros")
-# Input Backprop
+# IBP
 n.ibp(target = 4.0, epochs = 10000, learn_rate = .001, debug = True)
+"""
+OUTPUT
+------
+OPTIMAL INPUT       :: [[ 2.00003743  2.00003743]]
+CALCULATED OUT      :: [[ 4.00007486]]
+TARGET OUT          :: [[4.0]]
+TARGET vs CALC LOSS :: 7.48634338379e-05
+"""
+
+
+# Trial: 2
+# Inputs: 100
+# Learning Rate: .001
+# Epochs: 10000
+# Target: 4.0
+# FEED FORWARD
+n = Network([100, 1])
+n.initWeights(mode="ones")
+n.initBiases(mode="zeros")
+# IBP
+n.ibp(target = 4.0, epochs = 10000, learn_rate = .001, debug = True)
+"""
+OUTPUT
+------
+OPTIMAL INPUT       :: [[ 0.03999999  0.03999999  0.03999999  0.03999999  0.03999999  0.03999999
+                          0.03999999  0.03999999  0.03999999  0.03999999  0.03999999  0.03999999
+                          ...
+                          0.03999999  0.03999999  0.03999999  0.03999999]]
+CALCULATED OUT      :: [[ 3.99999666]]
+TARGET OUT          :: [[4.0]]
+TARGET vs CALC LOSS :: 3.33786010742e-06
+"""
+
+
+# Trial: 3
+# Inputs: 10
+# Learning Rate: .001
+# Epochs: 100000
+# Target: 1000.0
+# FEED FORWARD
+n = Network([10, 1])
+n.initWeights(mode="ones")
+n.initBiases(mode="zeros")
+# IBP
+n.ibp(target = 1000.0, epochs = 100000, learn_rate = .001, debug = True)
+"""
+OUTPUT
+------
+OPTIMAL INPUT       :: [[ 99.95669556  99.95669556  99.95669556  99.95669556  99.95669556
+                          99.95669556  99.95669556  99.95669556  99.95669556  99.95669556]]
+CALCULATED OUT      :: [[ 999.56689453]]
+TARGET OUT          :: [[1000.0]]
+TARGET vs CALC LOSS :: 0.43310546875
+"""
