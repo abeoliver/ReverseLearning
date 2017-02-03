81738264de4aa4f599f0a5bae712d223a43ecf16
Abraham Oliver <abeoliver.116@gmail.com>
2017-02-03 13:18:42 -0500

    Good models made, network edits made

--- Experiments/IrisTensor.py
+++ Experiments/IrisTensor.py
@@ -6,15 +6,13 @@ import numpy as np
 from math import floor
 from random import randint, random
 from csv import reader as CSV
-import sklearn as skl
-from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import StandardScaler
+from Network import Network
 
 def getDataset():
     # Prepare Data
     data = {"sepal length":[], "sepal width": [], "petal length": [],
             "petal width": [], "species": []}
-    with open("C:/Users/abeol/Git/ReverseLearning/DataSets/iris.tab") as tsv:
+    with open("iris.tab") as tsv:
         reader = CSV(tsv, dialect="excel-tab")
         for line in reader:
             if reader.line_num not in [1, 2, 3]:
@@ -71,44 +69,46 @@ def splitData(data, trainsize):
 full = getDataset()
 train, test = splitData(full, 140)
 
-scaler = StandardScaler()
-scaler.fit(train[0])
-XTrain = train[0]
-# XTrain = scaler.transform(train[0])
-
 def getAccuracy(classifier, testset):
     c = classifier
     runner = 0
     for i in range(len(testset[0])):
-        y_ = c.predict([testset[0][i]])[0]
+        y_ = c.feed([testset[0][i]])[0]
         compl = 0
         for j in range(len(y_)):
             if y_[j] == testset[1][i][j]: compl += 1
         if compl == len(y_): runner += 1
     return float(runner) / len(testset[0])
 
-c = MLPClassifier(hidden_layer_sizes = (),
-                   activation = "logistic",
-                   learning_rate = "constant",
-                   learning_rate_init = .001,
-                   solver = "lbfgs",
-                   verbose = False,
-                   max_iter = 10000000,
-                   tol = 1e-60)
-c.fit(XTrain, train[1])
-print(getAccuracy(c, [test[0], test[1]]))
-print(getAccuracy(c, [full[0], full[1]]))
-# print(getAccuracy(c, [scaler.transform(test[0]), test[1]]))
-# print(getAccuracy(c, [scaler.transform(full[0]), full[1]]))
+# ------------ USE IOA ON TRAINED NETWORK -----------
+n = Network([4, 2, 3], shaping="softmax")
+n.initWeights(mode="preset", preset=[[[-2.96867418, -2.04050994],
+                                      [-3.07419991, -1.34310222],
+                                      [5.77198744, 2.33850431],
+                                      [4.32180357, 2.56259775]],
+                                     [[-4.74904728, -3.10435867, 8.68291569],
+                                      [-1.60662615, -3.64023638, 2.82549834]]])
+n.initBiases(mode="preset", preset=[[[-6.04968405, -2.69198585]],
+                                    [[0.4100841, 2.19560456, -3.45070601]]])
 
-def predictTest(inp, lbl):
-    a = inp
-    # x = scaler.transform([inp])
-    x = [inp]
-    print("{0} --> {1}".format(a, x))
-    print("LABEL     :: {0}".format(lbl))
-    print("PREDICTED :: {0}".format(c.predict(x)[0]))
-# predictTest([5.9, 3.0, 5.1, 1.8], [0, 0, 1])
-# predictTest([6.0, 2.9, 4.5, 1.5], [0, 1, 0])
-# predictTest([5.3, 3.7, 1.5, 0.2], [1, 0, 0])
-# predictTest([5.2, 3.8, 1.6, 0.3], [1, 0, 0])
+optimal = n.ibp([1.0, 0.0, 0.0], epochs = -1, debug = False, learn_rate = .1,
+                error_tolerance = .01, rangeGradientScalar = 1e10,
+                restrictions = {0: (4.5, 8.0), 1: (2.0, 5.0), 2: (1.0, 6.5), 3: (0.0, 3.0)})
+
+# Check optimal
+print "OPTIMAL INPUTS :: {0}".format(n.eval(optimal))   # [[ 6.42741394  3.89656925  1.88798046  1.17135382 ]]
+print "OPTIMAL OUTPUT :: {0}".format(n.feed(optimal))   # [[ 0.99500012  0.00499985  0.                     ]]
+print "TARGET OUTPUT  :: {0}".format([1.0, 0.0, 0.0])   # [[1.0          0.          0.                     ]]
+
+# ------------------ TRAIN NETWORK ------------------
+# NOTE: The descovered weights and biases were manually entered
+#       into the above network after trained here
+#
+n = Network([4, 2, 3], shaping = "softmax")
+n.initWeights(mode = "random", mean = 0.0, stddev = 1.0)
+n.initBiases(mode = "random", mean = 0.0, stddev = 1.0)
+
+# n.train(data = full,
+#         epochs = 300000,
+#         learn_rate = .1)
+# ---------------------------------------------------
