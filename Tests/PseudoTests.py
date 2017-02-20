1944d068335073081380a0390c48c489196d4312
Abraham Oliver <abeoliver.116@gmail.com>
2017-02-20 15:15:21 -0500

    Added rounding

--- Tests/PseudoTests.py
+++ Tests/PseudoTests.py
@@ -4,15 +4,17 @@
 from random import random, randint, uniform
 import pandas
 
-def getPoints(function, amount, error, filename):
+def getPoints(function, amount, error, filename, roundX = False, roundY = False):
     xs = []
     ys = []
     for i in range(amount):
         newX = uniform(3, 58)
+        if roundX: newX = round(newX)
         effective = function(newX)
         newY = uniform(effective - error, effective + error)
+        if roundY: newY = round(newY)
         xs.append(newX)
-        ys.append(round(newY))
+        ys.append(newY)
     d = {"x": xs, "y": ys}
     df = pandas.DataFrame(d)
     df.to_csv("test", sep="\t")
