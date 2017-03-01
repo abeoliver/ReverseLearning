011fab815b01151db8c5842c5a536bb51b095a4c
Abraham Oliver <abeoliver.116@gmail.com>
2017-02-28 23:34:46 -0500

    Reran some tests

--- Tests/IOA Tests.py
+++ Tests/IOA Tests.py
@@ -39,18 +39,6 @@ def RUN(funcs, mode = ""):
             for func in funcs:
                 func()
 
-# LINEAR -- f(x) = 2x + 3
-def linear17():
-    title = "Linear Max -- f(x) = 2x + 3"
-    def f(x): return tf.add(tf.multiply(x, 2), 3)
-    I = IOA(f, 1)
-    final, digest = I.optimize(17.0, epochs = -1, learn_rate = .1, error_tolerance = .15,
-                               restrictions = {}, debug = True, debug_interval = -1,
-                               rangeGradientScalar = 1e11, gradientTolerance = 5e-7,
-                               startPreset = [], returnDigest = True, digestInterval = 1,
-                               title = title)
-    saveDigests(digest, "Records/Linear-17.ioa")
-
 # QUAD Positive -- f(x) = x^2 - 8x + 15
 def quadraticPositiveMin():
     title = "Quadratic Positive Min -- f(x) = x^2 - 8x + 15"
@@ -63,21 +51,9 @@ def quadraticPositiveMin():
                                title = title)
     saveDigests(digest, "Records/Quad-Positive-Min.ioa")
 
-# QUAD Negative -- f(x) = -x^2 - 3x + 18
-def quadraticNegativeMax():
-    title = "Quadratic Negative Max -- f(x) = -x^2 - 3x + 18"
-    def f(x): return tf.add(tf.add(-tf.pow(x, 2.0), tf.multiply(-3.0, x)), 18.0)
-    I = IOA(f, 1)
-    final, digest = I.optimize("max", epochs = -1, learn_rate = .1, error_tolerance = .15,
-                               restrictions = {}, debug = True, debug_interval = -1,
-                               rangeGradientScalar = 1e11, gradientTolerance = 1e-6,
-                               startPreset = [], returnDigest = True, digestInterval = 1,
-                               title = title)
-    saveDigests(digest, "Records/Quad-Negative-Max.ioa")
-
 # Surface #1 -- f(x, y) = (x - 2) ^ 2 + (y + 3) ^ 2
 def surface1():
-    title = "Surface #1 -- f(x, y) = x ^ 2 + y ^ 2 - 100"
+    title = "Surface #1 -- f(x, y) = (x - 2) ^ 2 + (y + 3) ^ 2"
     def f(x): return tf.add(tf.pow(tf.add(x[0][0], -2), 2), tf.pow(tf.add(x[0][1], 3), 2))
     I = IOA(f, 2)
     final, digest = I.optimize("min", epochs = -1, learn_rate = .1, error_tolerance = .15,
@@ -104,6 +80,22 @@ def manyVar():
                                title = title)
     saveDigests(digest, "Records/High-D-Surface.ioa")
 
+# Restricted Polynomial - f(x) = x^5 - 4x^4 - 7x^3 + 22x^2 + 24x
+def RestrictedPoly():
+    title = "Restricted Polynomial -- f(x) = x^5 - 4x^4 - 7x^3 + 22x^2 + 24x"
+    def f(x):
+        p1 = tf.add(tf.pow(x, 5.0), -tf.multiply(4.0, tf.pow(x, 4)))
+        p2 = tf.add(-tf.multiply(7.0, tf.pow(x, 3)), tf.multiply(22.0, tf.pow(x, 2)))
+        return tf.add(tf.multiply(24.0, x), tf.add(p2, p3))
+    I = IOA(f, 1)
+    final, digest = I.optimize("max", epochs = -1, learn_rate = .1, error_tolerance = .15,
+                               restrictions = {0: (0, 6)}, debug = True, debug_interval = -1,
+                               rangeGradientScalar = 1e11, gradientTolerance = 9e-6,
+                               startPreset = [],
+                               returnDigest = True, digestInterval = 1,
+                               title = title)
+    saveDigests(digest, "Records/Restricted-Polynomial.ioa")
+
 # Increasing Sin -- f(x) = sin(4x) + x
 def incSin():
     title = "Increasing Sin -- f(x) = sin(4x) + x"
@@ -117,10 +109,9 @@ def incSin():
 
 if __name__ == "__main__":
     funcs = []
-    # funcs.append(linear17)
-    # funcs.append(quadraticNegativeMax)
-    # funcs.append(quadraticPositiveMin)
-    # funcs.append(surface1)
-    # funcs.append(manyVar)
-    funcs.append(incSin)
+    funcs.append(quadraticPositiveMin)
+    funcs.append(surface1)
+    funcs.append(manyVar)
+    funcs.append(RestrictedPoly)
+    # funcs.append(incSin)
     RUN(funcs)
